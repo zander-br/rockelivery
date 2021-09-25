1bf7864016eefc76d9c8baa4cb445c4127af1d7f
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-09-25 11:56:22 -0300

    refactor: move function errors

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -1,5 +1,5 @@
 defmodule Rockelivery.Users.Create do
-  alias Rockelivery.{Repo, User}
+  alias Rockelivery.{Error, Repo, User}
 
   def call(params) do
     params
@@ -10,7 +10,5 @@ defmodule Rockelivery.Users.Create do
 
   defp handle_insert({:ok, %User{}} = result), do: result
 
-  defp handle_insert({:error, result}) do
-    {:error, %{status: :bad_request, result: result}}
-  end
+  defp handle_insert({:error, result}), do: {:error, Error.build(:bad_request, result)}
 end
