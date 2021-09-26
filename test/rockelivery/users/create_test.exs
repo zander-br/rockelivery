4c9b2b94a06f0fa97e336dcccbcfaa323c3861e8
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-09-26 07:42:19 -0300

    test: user create call/1 tests

---
+++ test/rockelivery/users/create_test.exs
@@ -0,0 +1,32 @@
+defmodule Rockelivery.Users.CreateTest do
+  use Rockelivery.DataCase, async: true
+
+  import Rockelivery.Factory
+
+  alias Rockelivery.{Error, User}
+  alias Rockelivery.Users.Create
+
+  describe "call/1" do
+    test "when all params are valid, returns the user" do
+      params = build(:user_params)
+
+      response = Create.call(params)
+
+      assert {:ok, %User{id: _id, name: "Joe Doe", email: "joe.doe@mail.com", age: 19}} = response
+    end
+
+    test "when there are invalid params, return an error" do
+      invalid_params = build(:user_params, %{age: 15, password: "123"})
+
+      response = Create.call(invalid_params)
+
+      expected_response = %{
+        age: ["must be greater than or equal to 18"],
+        password: ["should be at least 6 character(s)"]
+      }
+
+      assert {:error, %Error{status: :bad_request, result: changeset}} = response
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end
