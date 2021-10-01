86b641154389734bbabf59d77dad0be4562b7334
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-10-01 05:07:30 -0300

    test: renders user.json

--- test/rockelivery_web/views/users_view_test.exs
+++ test/rockelivery_web/views/users_view_test.exs
@@ -29,4 +29,26 @@ defmodule RockeliveryWeb.UsersViewTest do
              }
            } = response
   end
+
+  test "renders user.json" do
+    user = build(:user)
+
+    response = render(UsersView, "user.json", user: user)
+
+    assert %{
+             user: %User{
+               address: "valid address",
+               cep: "12345678",
+               age: 19,
+               cpf: "12345678900",
+               email: "joe.doe@mail.com",
+               id: "f62732cd-7b02-4594-8e64-d172299381a1",
+               inserted_at: nil,
+               name: "Joe Doe",
+               password: "valid_password",
+               password_hash: nil,
+               updated_at: nil
+             }
+           } = response
+  end
 end
