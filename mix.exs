7c9cb6afd824834469dfeb2f71703d3a7e2aac66
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-10-09 05:51:05 -0300

    chore: add bypass

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Rockelivery.MixProject do
       {:ex_machina, "~> 2.7.0"},
       {:decimal, "~> 2.0"},
       {:tesla, "~> 1.4.3"},
-      {:hackney, "~> 1.17"}
+      {:hackney, "~> 1.17"},
+      {:bypass, "~> 2.1", only: :test}
     ]
   end
 
