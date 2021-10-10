9f7bf0976c45659cc778aa8e5ce19817a7314251
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-10-10 10:41:46 -0300

    chore: add guardian

--- mix.exs
+++ mix.exs
@@ -52,13 +52,14 @@ defmodule Rockelivery.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:pbkdf2_elixir, "~> 1.4"},
       {:excoveralls, "~> 0.10", only: :test},
       {:ex_machina, "~> 2.7.0"},
       {:decimal, "~> 2.0"},
       {:tesla, "~> 1.4.3"},
       {:hackney, "~> 1.17"},
+      {:guardian, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:bypass, "~> 2.1", only: :test},
       {:mox, "~> 1.0", only: :test}
     ]
